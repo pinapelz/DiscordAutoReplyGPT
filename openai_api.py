bf7047a2e71719fae8ac151a8ec0846410be0008
Pinapelz <donaldshan1@outlook.com>
2023-05-23 11:15:50 -0700

    Fix bug with message_log not being tracked in OpenAI API

--- openai_api.py
+++ openai_api.py
@@ -4,8 +4,8 @@ from llm import LLM
 class OpenAIAPI(LLM):
     def __init__(self, api_key: str) -> None:
         super().__init__()
-        self.chat_log = []
-        self.api_key = api_key
+        openai.api_key = api_key
+        self._chat_log = []
 
     def get_response(self, message: str) -> str:
         if len(self._chat_log) >= 5:
@@ -17,6 +17,8 @@ class OpenAIAPI(LLM):
             model = "gpt-3.5-turbo",
             messages = payload
         )
+        self._chat_log += [{"role": "user", "content": message}, 
+                           {"role": "assistant", "content": response.choices[0].message["content"]}]
         print(response.choices[0].message["content"]) # type: ignore
         return response.choices[0].message["content"]
 
