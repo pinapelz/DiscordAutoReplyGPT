b277a2a72713c74d7adf2b3e006ea538f23a1013
Pinapelz <donaldshan1@outlook.com>
2023-05-23 12:29:29 -0700

    Fix incorrect indentation

--- gpt4free_api.py
+++ gpt4free_api.py
@@ -14,7 +14,7 @@ class GPT4FreeAPI(LLM):
             chat=self._chat_log)
         if len(self._chat_log) > 4:
             self._chat_log.pop(0)
-            self._chat_log.append({"question": message, "answer": response.text})
+        self._chat_log.append({"question": message, "answer": response.text})
         print(response.text)
         return response.text
     
